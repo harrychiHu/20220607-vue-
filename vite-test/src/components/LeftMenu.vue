c2fe9b6ff3356b8d0e7cccfd63e63524ec131ccf
harrychiHu <miyadodo1311@gmail.com>
2022-06-07 16:42:03 +0800

    add.file

---
+++ vite-test/src/components/LeftMenu.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+#left-menu
+  h1 社團介紹
+  table
+    tr(v-for="club in clubs")
+      td {{ club }}
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+
+const clubs = reactive([
+  '吉他', '熱舞', '棒球', '羽球', '足球', '童軍'
+])
+</script>
+
+<!-- <script>
+import { reactive } from 'vue'
+export default {
+  setup () {
+    const clubs = reactive([
+      '吉他', '熱舞', '棒球', '羽球', '足球', '童軍'
+    ])
+    return {
+      clubs
+    }
+  }
+}
+</script> -->
